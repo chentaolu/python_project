d64411be01399f3dfe1e9b1da1a246afed6b313c
YunHaoW <d0786880@mail.fcu.edu.tw>
2021-04-11 15:03:29 +0800

    add comments

--- project.py
+++ project.py
@@ -11,9 +11,11 @@ import matplotlib.pyplot as plt
 marriedData = pd.read_excel('結婚人數.xlsx')
 aboardData = pd.read_excel('歷年中華民國國民出國按年齡分.xlsx')
 
+#整理結婚人數資料
 marriedData = marriedData.drop(['結婚人數按性別、五歲年齡組及結婚次數分'], axis = 1)
 marriedData = marriedData.drop([0, 3, 143, 144], axis = 0)
 
+#整理出國人數資料
 aboardData.columns = ['year', 'total', '12-', '13~19', '20~29', '30~39', '40~49', '50~59', '60+']
 aboardData = aboardData.drop([0], axis = 0)
 
