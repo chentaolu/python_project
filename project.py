cc4fb8517c34186d68c45817b38759a3c4af2564
YunHaoW <d0786880@mail.fcu.edu.tw>
2021-04-11 15:00:46 +0800

    arrange aboardData

--- project.py
+++ project.py
@@ -10,3 +10,15 @@ import matplotlib.pyplot as plt
 
 marriedData = pd.read_excel('結婚人數.xlsx')
 aboardData = pd.read_excel('歷年中華民國國民出國按年齡分.xlsx')
+
+marriedData = marriedData.drop(['結婚人數按性別、五歲年齡組及結婚次數分'], axis = 1)
+marriedData = marriedData.drop([0, 3, 143, 144], axis = 0)
+
+aboardData.columns = ['year', 'total', '12-', '13~19', '20~29', '30~39', '40~49', '50~59', '60+']
+aboardData = aboardData.drop([0], axis = 0)
+
+for i in range(1, len(aboardData.index)+1):
+    aboardData['year'].loc[i] = int(str(aboardData['year'].loc[i])[-4:])
+aboardData = aboardData.set_index('year')
+
+print(marriedData)
